e312002f73e940c6d8c1775269bb4eb52a0c0a5c
Dmitry Tychko <dmitrytychko@gmail.com>
2021-06-02 17:15:57 +0300

    Add initial implementation of BalancedQueue with partition processing limit

---
+++ src/RabbitMqAsyncPublisher/BalancedQueue.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace RabbitMqAsyncPublisher
+{
+    public class BalancedQueue<TValue>
+    {
+        private readonly AsyncManualResetEvent _gateEvent = new AsyncManualResetEvent(false);
+        private readonly int _partitionProcessingLimit;
+
+        private readonly ConcurrentDictionary<string, Partition> _partitions =
+            new ConcurrentDictionary<string, Partition>();
+
+        private readonly ConcurrentQueue<Partition> _partitionQueue = new ConcurrentQueue<Partition>();
+        private volatile int _partitionQueueCount;
+
+        public BalancedQueue(int partitionProcessingLimit)
+        {
+            _partitionProcessingLimit = partitionProcessingLimit;
+        }
+
+        public void Enqueue(string partitionKey, TValue value)
+        {
+            var partition = _partitions.GetOrAdd(partitionKey, _ => new Partition(partitionKey));
+            var shouldEnqueuePartition = false;
+
+            lock (partition)
+            {
+                partition.Queue.Enqueue(value);
+
+                if (partition.Queue.Count == 1 && partition.ProcessingCount < _partitionProcessingLimit)
+                {
+                    shouldEnqueuePartition = true;
+                }
+            }
+
+            if (shouldEnqueuePartition)
+            {
+                _partitionQueue.Enqueue(partition);
+                Interlocked.Increment(ref _partitionQueueCount);
+                AdjustGate();
+            }
+        }
+
+        public async Task WaitToDequeueAsync(CancellationToken cancellationToken = default)
+        {
+            await _gateEvent.WaitAsync(cancellationToken);
+        }
+
+        public bool TryDequeue(Func<TValue, string, Task> handle)
+        {
+            if (!_partitionQueue.TryDequeue(out var partition))
+            {
+                AdjustGate();
+                return false;
+            }
+
+            Interlocked.Decrement(ref _partitionQueueCount);
+            AdjustGate();
+
+            var shouldEnqueuePartition = false;
+            TValue value;
+
+            lock (partition)
+            {
+                value = partition.Queue.Dequeue();
+                partition.ProcessingCount += 1;
+
+                if (partition.Queue.Count > 0 && partition.ProcessingCount < _partitionProcessingLimit)
+                {
+                    shouldEnqueuePartition = true;
+                }
+            }
+
+            if (shouldEnqueuePartition)
+            {
+                _partitionQueue.Enqueue(partition);
+                Interlocked.Increment(ref _partitionQueueCount);
+                AdjustGate();
+            }
+
+            HandleSafe(handle, value, partition);
+            return true;
+        }
+
+        private async void HandleSafe(Func<TValue, string, Task> handle, TValue value, Partition partition)
+        {
+            try
+            {
+                await handle(value, partition.Name).ConfigureAwait(false);
+            }
+            catch
+            {
+                // TODO: Log unexpected error
+            }
+
+            var shouldEnqueuePartition = false;
+
+            lock (partition)
+            {
+                partition.ProcessingCount -= 1;
+
+                if (partition.Queue.Count > 0 && partition.ProcessingCount == _partitionProcessingLimit - 1)
+                {
+                    shouldEnqueuePartition = true;
+                }
+            }
+
+            if (shouldEnqueuePartition)
+            {
+                _partitionQueue.Enqueue(partition);
+                Interlocked.Increment(ref _partitionQueueCount);
+                AdjustGate();
+            }
+        }
+
+        private void AdjustGate()
+        {
+            lock (_gateEvent)
+            {
+                if (_partitionQueueCount > 0)
+                {
+                    _gateEvent.Set();
+                }
+                else
+                {
+                    _gateEvent.Reset();
+                }
+            }
+        }
+
+        private class Partition
+        {
+            public readonly string Name;
+            public readonly Queue<TValue> Queue = new Queue<TValue>();
+            public int ProcessingCount;
+
+            public Partition(string name)
+            {
+                Name = name;
+            }
+        }
+    }
+}
---
+++ src/RabbitMqAsyncPublisher/Tests/BalancedQueueTests.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using RabbitMqAsyncPublisher;
+using Shouldly;
+
+namespace Tests
+{
+    [TestFixture]
+    public class BalancedQueueTests
+    {
+        [Test]
+        [Repeat(2)]
+        [Timeout(5000)]
+        public async Task RandomTest(
+            [Values(1, 10, 100)] int partitionProcessingLimit,
+            [Values(100_000)] int valueCount,
+            [Values(1, 5, 10, 20)] int writerCount,
+            [Values(5, 10)] int readerCount)
+        {
+            ThreadPool.SetMaxThreads(100, 100);
+            ThreadPool.SetMinThreads(100, 100);
+
+            var random = new Random(123);
+            var mre = new ManualResetEventSlim(false);
+
+            var queue = new BalancedQueue<int>(partitionProcessingLimit);
+
+            var writersReadyEvent = new CountdownEvent(writerCount);
+            var readersReadyEvent = new CountdownEvent(readerCount);
+
+            var writers = new List<Task>();
+            var valuesPerWriter = valueCount / writerCount;
+
+            for (var i = 0; i < writerCount; i++)
+            {
+                writers.Add(Task.Run(() =>
+                {
+                    writersReadyEvent.Signal();
+                    mre.Wait();
+
+                    for (var j = 0; j < valuesPerWriter; j++)
+                    {
+                        queue.Enqueue($"partition#{random.Next(10)}", j);
+                    }
+                }));
+            }
+
+            var readers = new List<Task<string[]>>();
+            var cts = new CancellationTokenSource();
+            var counter = 0;
+
+            for (var i = 0; i < readerCount; i++)
+            {
+                readers.Add(Task.Run(async () =>
+                {
+                    var values = new ConcurrentQueue<string>();
+
+                    try
+                    {
+                        readersReadyEvent.Signal();
+                        mre.Wait();
+
+                        while (!cts.IsCancellationRequested)
+                        {
+                            await queue.WaitToDequeueAsync(cts.Token).ConfigureAwait(false);
+
+                            while (queue.TryDequeue(async (value, partitionKey) =>
+                            {
+                                await Task.Run(() =>
+                                {
+                                    values.Enqueue(partitionKey);
+                                    Interlocked.Increment(ref counter);
+
+                                    if (counter == writerCount * valuesPerWriter)
+                                    {
+                                        cts.Cancel();
+                                    }
+                                }).ConfigureAwait(false);
+                            }))
+                            {
+                            }
+                        }
+                    }
+                    catch (OperationCanceledException)
+                    {
+                        // Ignore
+                    }
+
+                    return values.ToArray();
+                }, cts.Token));
+            }
+
+            readersReadyEvent.Wait();
+            writersReadyEvent.Wait();
+
+            var stopwatch = Stopwatch.StartNew();
+            mre.Set();
+
+            await Task.WhenAll(writers).ConfigureAwait(false);
+
+            Task.Delay(1000).ContinueWith(_ => { Console.WriteLine(counter); }, cts.Token);
+
+            var results = await Task.WhenAll(readers).ConfigureAwait(false);
+            var resultCount = results.Aggregate(0, (count, arr) => count + arr.Length);
+
+            Console.WriteLine($"Total = {resultCount}");
+            Console.WriteLine($"Duration = {stopwatch.ElapsedMilliseconds} ms");
+
+            resultCount.ShouldBe(writerCount * valuesPerWriter);
+
+            // foreach (var result in results)
+            // {
+            //     Console.WriteLine("=======================================================");
+            //     Console.WriteLine($"Total = {result.Length}");
+            //     Console.WriteLine(string.Join("; ",
+            //         result.GroupBy(x => x)
+            //             .OrderBy(x => x.Key)
+            //             .Select(x => $"{x.Key} = {x.Count()}")));
+            // }
+        }
+    }
+}
